1ac0d2af0eae5e2c9ff52969d44ce9b2be35a303
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2020-06-19 12:19:57 -0300

    Covered until 'Wiring Actions to a Component'

--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -2,7 +2,7 @@ import { map } from 'rxjs/operators';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { Customer, Project, ProjectsService, NotificationsService, CustomersService, ProjectsState } from '@workshop/core-data';
+import { Customer, Project, ProjectsService, NotificationsService, CustomersService, ProjectsState, AddProject, UpdateProject, DeleteProject } from '@workshop/core-data';
 
 const emptyProject: Project = {
   id: null,
@@ -69,30 +69,27 @@ export class ProjectsComponent implements OnInit {
   }
 
   createProject(project) {
-    this.projectsService.create(project)
-      .subscribe(response => {
-        this.ns.emit('Project created!');
-        this.getProjects();
-        this.resetCurrentProject();
-      });
+    this.store.dispatch(new AddProject(project));
+
+    // These will go away
+    this.ns.emit('Project created!');
+    this.resetCurrentProject();
   }
 
   updateProject(project) {
-    this.projectsService.update(project)
-      .subscribe(response => {
-        this.ns.emit('Project saved!');
-        this.getProjects();
-        this.resetCurrentProject();
-      });
+    this.store.dispatch(new UpdateProject(project));
+    
+    // These will go away
+    this.ns.emit('Project saved!');
+    this.resetCurrentProject();
   }
 
   deleteProject(project) {
-    this.projectsService.delete(project)
-      .subscribe(response => {
-        this.ns.emit('Project deleted!');
-        this.getProjects();
-        this.resetCurrentProject();
-      });
+    this.store.dispatch(new DeleteProject(project));
+    
+    // These will go away
+    this.ns.emit('Project deleted!');
+    this.resetCurrentProject();
   }
 }
 
--- libs/core-data/src/lib/state/projects/projects.reducer.ts
+++ libs/core-data/src/lib/state/projects/projects.reducer.ts
@@ -1,4 +1,5 @@
 import { Project } from './../../projects/project.model';
+import { ProjectsActionTypes } from './projects.actions';
 
 const initialProjects: Project[] = [
   {
@@ -48,8 +49,28 @@ export const initialState: ProjectsState = {
 // 03 Build the MOST simplest reducer
 export function projectsReducers(
   state = initialState, action): ProjectsState {
-    switch(action.type) {
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case ProjectsActionTypes.ProjectSelected:
+      return {
+        selectedProjectId: action.payload,
+        projects: state.projects
+      }
+    case ProjectsActionTypes.AddProject:
+      return {
+        selectedProjectId: state.selectedProjectId,
+        projects: createProject(state.projects, action.payload)
+      }
+    case ProjectsActionTypes.UpdateProject:
+      return {
+        selectedProjectId: state.selectedProjectId,
+        projects: updateProject(state.projects, action.payload)
+      }
+    case ProjectsActionTypes.DeleteProject:
+      return {
+        selectedProjectId: state.selectedProjectId,
+        projects: deleteProject(state.projects, action.payload)
+      }
+    default:
+      return state;
+  }
 }
